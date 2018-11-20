bb325a6de4aff754aed100e4c2404b19ea1438ad
unknown <brianmunyalomalio8@gmail.com>
2018-11-20 21:17:52 +0300

    first commit

--- JAVAEXERCISE/areAnagram.java
+++ JAVAEXERCISE/areAnagram.java
@@ -1,25 +1,31 @@
-   //1. Write a function to detect if two strings are anagrams(3 marks).
-   //ANSWER:
-   
-   static boolean areAnagram(char[] str1, char[] str2) 
-    { //anagrams are strings that can be rearranged to look the same. eg Dormitory and Dirtyroom
-        // Get lenghts of both strings inputs
-        int string1len = str1.length; 
-        int string2len = str2.length; 
-   
-        // If length of both strings input is not same, 
-        // then they cannot be anagram 
-        if (string1len != string2len) 
-            return false; 
-   
-        // Sort both strings 
-        quickSort(str1, 0, string1len - 1); 
-        quickSort(str2, 0, string2len - 1); 
-   
-        // Compare sorted strings 
-        for (int i = 0; i < string1len;  i++) 
-            if (str1[i] != str2[i]) 
-                return false; 
-   
-        return true; 
-    } 
+//Write a function to check whether two strings are anagrams.
+import java.util.Arrays;
+public class AnagramSample  {
+   public static void areAnagram(String args[]) {
+      String str1 = "recitals";
+      String str2 = "articles";
+
+      if (str1.length()==str2.length()) 
+      {      
+         //sort the array represented by str1
+         char[] arr1 = str1.toCharArray();
+         Arrays.sort(arr1);
+         System.out.println(Arrays.toString(arr1));
+        //sort the array represented by str2
+         char[] arr2 = str2.toCharArray();
+         Arrays.sort(arr2);
+         System.out.println(Arrays.toString(arr2));
+        //check whether the sorted arrays are equal
+         System.out.println(Arrays.equals(arr1, arr2));
+        //if they are equal they are anagrams
+         if(arr1.equals(arr2)) 
+         {
+            System.out.println("Given strings are anagrams");
+         } 
+         //else they are not anagrams
+         else {
+            System.out.println("Given strings are not anagrams");
+         }
+      }
+   }
+}
